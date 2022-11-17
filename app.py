4d391ff0ea85fa5f32c479fe3005e32436c269f7
lucio.fondon <lucio.fondon@sansano.usm.cl>
2022-11-17 00:36:13 -0300

    a

--- app.py
+++ app.py
@@ -64,8 +64,8 @@ def get_inmobiliarias_sii():
         
     return {'data': data}, 200 
 
-@app.route('/v1/getRecommendations', methods=['POST'])
-def get_recommendation():
+@app.route('/v1/getPropRecommendations', methods=['POST'])
+def get_prop_recommendation():
     # hay que pasarle datos de esta manera
     # {
     # "historial": [{"titulo": "casa1", "comuna": "Ñuñoa", "latitude": 10, "longitude": 20, "habitaciones": 1, "banos": 2010, "precio": 1000, "tipoVenta": "Arriendo", "superficie": 50},
@@ -85,12 +85,12 @@ def get_recommendation():
     historial_propiedades = json_data['historial']
     total_propiedades = json_data['propiedades']
 
-    print("historial propiedades tipo", type(historial_propiedades))
-    print("historial propiedades", historial_propiedades)
+    # print("historial propiedades tipo", type(historial_propiedades))
+    # print("historial propiedades", historial_propiedades)
 
-    print("total propiedades tipo", type(total_propiedades))
-    print("total propiedades", total_propiedades)
-    # print(total_propiedades)
+    # print("total propiedades tipo", type(total_propiedades))
+    # print("total propiedades", total_propiedades)
+    # # print(total_propiedades)
 
     # removemos del total de propiedades las que están en el historial
     for propiedad in historial_propiedades:
@@ -140,6 +140,124 @@ def get_recommendation():
     # print(jsonify(total_propiedades.sort_values(by='score').to_dict(orient='records')))
 
     return jsonify(total_propiedades.sort_values(by='score').to_dict(orient='records'))
+
+@app.route('/v1/getUserRecommendations', methods=['POST'])
+def get_user_recommendation():
+    json_data = json.loads(request.data)
+
+    # guardamos los datos de la request
+    usuario = json_data['usuario']
+    propiedades = json_data['propiedades']
+    usuarios = json_data['usuarios']
+
+    # usuario = {"nombre": "Lucio",
+    #         "historial": [{'titulo': "casa1", 'comuna': "Ñuñoa", 'latitude': 10, 'longitude': 20, 'habitaciones': 1, 'banos': 2010, 'precio': 1000, 'tipoVenta': "Arriendo", 'superficie': 50}, 
+    #                         {'titulo': "casa2", 'comuna': "Ñuñoa", 'latitude': 10, 'longitude': 20, 'habitaciones': 1, 'banos': 2010, 'precio': 2000, 'tipoVenta': "Arriendo", 'superficie': 50}, 
+    #                         {'titulo': "casa3", 'comuna': "Ñuñoa", 'latitude': 10, 'longitude': 20, 'habitaciones': 1, 'banos': 2010, 'precio': 1200, 'tipoVenta': "Arriendo", 'superficie': 50}, 
+    #                         {'titulo': "casa4", 'comuna': "Ñuñoa", 'latitude': 10, 'longitude': 20, 'habitaciones': 1, 'banos': 2010, 'precio': 1500, 'tipoVenta': "Arriendo", 'superficie': 50}]
+    # }
+
+    # propiedades = [{'titulo': "casa4", 'comuna': "Ñuñoa", 'latitude': 10, 'longitude': 20, 'habitaciones': 1, 'banos': 2010, 'precio': 1500, 'tipoVenta': "Arriendo", 'superficie': 50},
+    # {'titulo': "casa5", 'comuna': "Santiago", 'latitude': -100, 'longitude': 200, 'habitaciones': 5, 'banos': 20100, 'precio': 100000, 'tipoVenta': "Arriendo", 'superficie': 500}, 
+    # {'titulo': "casa6", 'comuna': "Macul", 'latitude': 15, 'longitude': 25, 'habitaciones': 6, 'banos': 201000, 'precio': 1000000, 'tipoVenta': "Arriendo", 'superficie': 5000}, 
+    # {'titulo': "casa7", 'comuna': "Ñuñoa", 'latitude': 10, 'longitude': 20, 'habitaciones': 1, 'banos': 2010, 'precio': 1300, 'tipoVenta': "Arriendo", 'superficie': 50}, 
+    # {'titulo': "casa8", 'comuna': "San Joaquin", 'latitude': 100, 'longitude': 200, 'habitaciones': 9, 'banos': 2010000, 'precio': 10000, 'tipoVenta': "Venta", 'superficie': 500000}]
+
+    # usuarios = [{"nombre": "Lucio",
+    #         "historial": [{'titulo': "casa1", 'comuna': "Ñuñoa", 'latitude': 10, 'longitude': 20, 'habitaciones': 1, 'banos': 2010, 'precio': 1000, 'tipoVenta': "Arriendo", 'superficie': 50}, 
+    #                         {'titulo': "casa2", 'comuna': "Ñuñoa", 'latitude': 10, 'longitude': 20, 'habitaciones': 1, 'banos': 2010, 'precio': 2000, 'tipoVenta': "Arriendo", 'superficie': 50}, 
+    #                         {'titulo': "casa3", 'comuna': "Ñuñoa", 'latitude': 10, 'longitude': 20, 'habitaciones': 1, 'banos': 2010, 'precio': 1200, 'tipoVenta': "Arriendo", 'superficie': 50}, 
+    #                         {'titulo': "casa4", 'comuna': "Ñuñoa", 'latitude': 10, 'longitude': 20, 'habitaciones': 1, 'banos': 2010, 'precio': 1500, 'tipoVenta': "Arriendo", 'superficie': 50}]
+    # },
+    # {"nombre": "Lucio2",
+    #         "historial":
+    # [{'titulo': "casa4", 'comuna': "Ñuñoa", 'latitude': 10, 'longitude': 20, 'habitaciones': 1, 'banos': 2010, 'precio': 1500, 'tipoVenta': "Arriendo", 'superficie': 50},
+    # {'titulo': "casa5", 'comuna': "Santiago", 'latitude': -100, 'longitude': 200, 'habitaciones': 5, 'banos': 20100, 'precio': 100000, 'tipoVenta': "Arriendo", 'superficie': 500}, 
+    # {'titulo': "casa6", 'comuna': "Macul", 'latitude': 15, 'longitude': 25, 'habitaciones': 6, 'banos': 201000, 'precio': 1000000, 'tipoVenta': "Arriendo", 'superficie': 5000}, 
+    # {'titulo': "casa7", 'comuna': "Ñuñoa", 'latitude': 10, 'longitude': 20, 'habitaciones': 1, 'banos': 2010, 'precio': 1300, 'tipoVenta': "Arriendo", 'superficie': 50}, 
+    # {'titulo': "casa8", 'comuna': "San Joaquin", 'latitude': 100, 'longitude': 200, 'habitaciones': 9, 'banos': 2010000, 'precio': 10000, 'tipoVenta': "Venta", 'superficie': 500000}],
+    # },
+    # {"nombre": "Lucio3",
+    #         "historial":
+    # [{'titulo': "casa4", 'comuna': "Ñuñoa", 'latitude': 10, 'longitude': 20, 'habitaciones': 1, 'banos': 2010, 'precio': 1500, 'tipoVenta': "Arriendo", 'superficie': 50},
+    # {'titulo': "casa5", 'comuna': "Santiago", 'latitude': -100, 'longitude': 200, 'habitaciones': 5, 'banos': 20100, 'precio': 100000, 'tipoVenta': "Arriendo", 'superficie': 500}, 
+    # {'titulo': "casa6", 'comuna': "Macul", 'latitude': 15, 'longitude': 25, 'habitaciones': 6, 'banos': 201000, 'precio': 1000000, 'tipoVenta': "Arriendo", 'superficie': 5000}, 
+    # {'titulo': "casa7", 'comuna': "Ñuñoa", 'latitude': 10, 'longitude': 20, 'habitaciones': 1, 'banos': 2010, 'precio': 1300, 'tipoVenta': "Arriendo", 'superficie': 50}, 
+    # {'titulo': "casa8", 'comuna': "San Joaquin", 'latitude': 100, 'longitude': 200, 'habitaciones': 9, 'banos': 2010000, 'precio': 10000, 'tipoVenta': "Venta", 'superficie': 500000}],
+    # },
+    # {"nombre": "Lucio4",
+    #         "historial":
+    # [{'titulo': "casa4", 'comuna': "Ñuñoa", 'latitude': 10, 'longitude': 20, 'habitaciones': 1, 'banos': 2010, 'precio': 1500, 'tipoVenta': "Arriendo", 'superficie': 50},
+    # {'titulo': "casa5", 'comuna': "Santiago", 'latitude': -100, 'longitude': 200, 'habitaciones': 5, 'banos': 20100, 'precio': 100000, 'tipoVenta': "Arriendo", 'superficie': 500}, 
+    # {'titulo': "casa6", 'comuna': "Macul", 'latitude': 15, 'longitude': 25, 'habitaciones': 6, 'banos': 201000, 'precio': 1000000, 'tipoVenta': "Arriendo", 'superficie': 5000}, 
+    # {'titulo': "casa7", 'comuna': "Ñuñoa", 'latitude': 10, 'longitude': 20, 'habitaciones': 1, 'banos': 2010, 'precio': 1300, 'tipoVenta': "Arriendo", 'superficie': 50}, 
+    # {'titulo': "casa8", 'comuna': "San Joaquin", 'latitude': 100, 'longitude': 200, 'habitaciones': 9, 'banos': 2010000, 'precio': 10000, 'tipoVenta': "Venta", 'superficie': 500000}]
+    # }
+    # ]
+
+    df = pd.DataFrame(propiedades)
+    df = df[['latitude', 'longitude', 'tipoVenta', 'habitaciones', 'banos', 'superficie', 'precio']].replace("Arriendo", 0).replace("Venta", 1)
+
+    scaler = MinMaxScaler()
+    scaler.fit(df)
+
+    def getScore(user, user2):
+        user2Aux = user2
+        metadata = pd.DataFrame(user['historial'])
+        metadata2 = pd.DataFrame(user2['historial'])
+
+        m = metadata.shape[0]
+        dfAux = pd.concat([metadata, metadata2])
+        dfAux = dfAux[['latitude', 'longitude', 'tipoVenta', 'habitaciones', 'banos', 'superficie', 'precio']].replace("Arriendo", 0).replace("Venta", 1)
+
+        scaled = scaler.fit_transform(dfAux)
+        scaled_df = pd.DataFrame(scaled, columns=dfAux.columns)
+
+        meta = scaled_df.iloc[:m,:]
+        meta2 = scaled_df.iloc[m:,:]
+
+        meanX = meta['latitude'].mean()
+        meanY = meta['longitude'].mean()
+        meanT = meta['tipoVenta'].mean()
+        meanH = meta['habitaciones'].mean()
+        meanB = meta['banos'].mean()
+        meanS = meta['superficie'].mean()
+        meanP = meta['precio'].mean()
+
+        meanX2 = meta2['latitude'].mean()
+        meanY2 = meta2['longitude'].mean()
+        meanT2 = meta2['tipoVenta'].mean()
+        meanH2 = meta2['habitaciones'].mean()
+        meanB2 = meta2['banos'].mean()
+        meanS2 = meta2['superficie'].mean()
+        meanP2 = meta2['precio'].mean()
+
+        user2Aux['score'] = abs(meanX-meanX2)*10 + abs(meanY-meanY2)*10 + abs(meanT-meanT2)*30 + abs(meanH-meanH2)*10 + abs(meanB-meanB2)*10 + abs(meanS-meanS2)*10 + abs(meanP-meanP2)*20
+
+        return user2Aux
+
+    usersWithScore = list()
+
+    for usuario2 in usuarios:
+        usersWithScore.append(getScore(usuario, usuario2))
+
+    df_final = pd.DataFrame(usersWithScore).sort_values(by='score')
+
+    df_final = df_final.fillna(0)
+    df_final = df_final.iloc[1:,:]
+    df_final = df_final[df_final['tipo'] == 'Particular']
+    # print(total_propiedades)
+    #
+    # revisar cuantos datos faltan por columna
+    # print("Número de datos NaN por feature Test: \n")
+    # print(total_propiedades.isnull().sum(axis = 0))
+
+    # print(jsonify(total_propiedades.sort_values(by='score').to_dict(orient='records')))
+    # print(jsonify(total_propiedades.sort_values(by='score').to_dict(orient='records')))
+
+    return jsonify(df_final.sort_values(by='score').to_dict(orient='records'))
+
+
 
 if __name__ == "__main__":  
     app.run()
